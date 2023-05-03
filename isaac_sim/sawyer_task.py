66b3711f85d50a49f59f7f5b192f894934d88135
Xavier Barneclo <xavierbarneclo@berkeley.edu>
2023-05-02 18:50:27 -0700

    small changes

--- isaac_sim/sawyer_task.py
+++ isaac_sim/sawyer_task.py
@@ -4,8 +4,9 @@ from omni.isaac.core.tasks.base_task import BaseTask
 from omni.isaac.core.articulations import ArticulationView
 from omni.isaac.core.utils.prims import create_prim
 from omni.timeline.ITimeline import get_current_time
-
 from omni.isaac.core.utils.viewports import set_camera_view
+
+from trajectories import LinearTrajectory, CircularTrajectory
 
 import gym
 from gym import spaces
@@ -26,7 +27,7 @@ class SawyerTask(BaseTask):
         self._max_torque = 10
 
         # values used for defining RL buffers
-        self._num_observations = 21
+        self._num_observations = 24
         self._num_actions = 7
         self._device = "cpu"
         self.num_envs = 1
@@ -38,6 +39,13 @@ class SawyerTask(BaseTask):
         # set the action and observation space for RL
         self.action_space = spaces.Box(np.ones(self._num_actions) * -1.0, np.ones(self._num_actions) * 1.0)
         self.observation_space = spaces.Box(np.ones(self._num_observations) * -np.Inf, np.ones(self._num_observations) * np.Inf)
+
+        # logistic smoothing kernel sensitiity parameter and relevance parameter for position vs velocity tracking
+        self.l = 0.5
+        self.w = 0.75
+
+        # reset max joint velocity
+        self._reset_vel = 1.328
 
         # trigger __init__ of parent class
         BaseTask.__init__(self, name=name, offset=offset)
@@ -66,13 +74,13 @@ class SawyerTask(BaseTask):
         set_camera_view(eye=camera_position, target=camera_target, camera_prim_path="/OmniverseKit_Persp")
 
     def post_reset(self):
-        self._j0_dof_idx = self._sawyer.get_dof_index("right_j0")
-        self._j1_dof_idx = self._sawyer.get_dof_index("right_j1")
-        self._j2_dof_idx = self._sawyer.get_dof_index("right_j2")
-        self._j3_dof_idx = self._sawyer.get_dof_index("right_j3")
-        self._j4_dof_idx = self._sawyer.get_dof_index("right_j4")
-        self._j5_dof_idx = self._sawyer.get_dof_index("right_j5")
-        self._j6_dof_idx = self._sawyer.get_dof_index("right_j6")
+
+        self._joint_indices = torch.zeros(7)
+
+        para = "right_j"
+        for i in range(7):
+            tpara = para + str(i)
+            self._joint_indices[i] = self._sawyer.get_dof_index(tpara)
 
         # randomize all envs
         indices = torch.arange(self._sawyer.count, dtype=torch.int64, device=self._device)
@@ -85,13 +93,12 @@ class SawyerTask(BaseTask):
 
         # randomize DOF positions
         dof_pos = torch.zeros((num_resets, self._sawyer.num_dof), device=self._device)
-        dof_pos[:, self._j0_dof_idx] = (self._sawyer.get_dof_limits("right_j0")[0] - self._sawyer.get_dof_limits("right_j0")[1]) * torch.rand(num_resets, device=self._device) + self._sawyer.get_dof_limits("right_j0")[1]
-        dof_pos[:, self._j1_dof_idx] = (self._sawyer.get_dof_limits("right_j1")[0] - self._sawyer.get_dof_limits("right_j1")[1]) * torch.rand(num_resets, device=self._device) + self._sawyer.get_dof_limits("right_j1")[1]
-        dof_pos[:, self._j2_dof_idx] = (self._sawyer.get_dof_limits("right_j2")[0] - self._sawyer.get_dof_limits("right_j2")[1]) * torch.rand(num_resets, device=self._device) + self._sawyer.get_dof_limits("right_j2")[1]
-        dof_pos[:, self._j3_dof_idx] = (self._sawyer.get_dof_limits("right_j3")[0] - self._sawyer.get_dof_limits("right_j3")[1]) * torch.rand(num_resets, device=self._device) + self._sawyer.get_dof_limits("right_j3")[1]
-        dof_pos[:, self._j4_dof_idx] = (self._sawyer.get_dof_limits("right_j4")[0] - self._sawyer.get_dof_limits("right_j4")[1]) * torch.rand(num_resets, device=self._device) + self._sawyer.get_dof_limits("right_j4")[1]
-        dof_pos[:, self._j5_dof_idx] = (self._sawyer.get_dof_limits("right_j5")[0] - self._sawyer.get_dof_limits("right_j5")[1]) * torch.rand(num_resets, device=self._device) + self._sawyer.get_dof_limits("right_j5")[1]
-        dof_pos[:, self._j6_dof_idx] = (self._sawyer.get_dof_limits("right_j6")[0] - self._sawyer.get_dof_limits("right_j6")[1]) * torch.rand(num_resets, device=self._device) + self._sawyer.get_dof_limits("right_j6")[1]
+
+        for i in range(7):
+            jo = self._joint_indices[i]
+            upper = self._sawyer.get_dof_limits()[:, jo, 1]
+            lower = self._sawyer.get_dof_limits()[:, jo, 0]
+            dof_pos[:, jo] = (upper - lower) * torch.rand(num_resets, device=self._device) + lower
 
         # zero all DOF velocities
         dof_vel = torch.zeros((num_resets, self._sawyer.num_dof), device=self._device)
@@ -106,6 +113,13 @@ class SawyerTask(BaseTask):
 
         self.start_time = get_current_time()
 
+        x = np.random.rand(2,1) * 20 - 10
+        y = np.random.rand(2,1) * 15 + 5
+        z = np.random.rand(2,1) * 12 + 3
+
+        pt = np.hstack((x, y ,z))
+        self.trajectory = LinearTrajectory(pt[0], pt[1], 5)
+
     def pre_physics_step(self, actions) -> None:
         reset_env_ids = self.resets.nonzero(as_tuple=False).squeeze(-1)
         if len(reset_env_ids) > 0:
@@ -118,13 +132,9 @@ class SawyerTask(BaseTask):
         self._max_torque = self._sawyer.get_max_efforts(joint_indices=torch.tensor([self._j0_dof_idx, self._j1_dof_idx, self._j2_dof_idx, self._j3_dof_idx, self._j4_dof_idx, self._j5_dof_idx, self._j6_dof_idx]))
 
         # add indexes to max_torque based on correct joints
-        forces[:, self._j0_dof_idx] = self._max_torque[0] * actions[0]
-        forces[:, self._j1_dof_idx] = self._max_torque[1] * actions[1]
-        forces[:, self._j2_dof_idx] = self._max_torque[2] * actions[2]
-        forces[:, self._j3_dof_idx] = self._max_torque[3] * actions[3]
-        forces[:, self._j4_dof_idx] = self._max_torque[4] * actions[4]
-        forces[:, self._j5_dof_idx] = self._max_torque[5] * actions[5]
-        forces[:, self._j6_dof_idx] = self._max_torque[6] * actions[6]
+        for i in range(7):
+            jo = self._joint_indices[i]
+            forces[:, jo] = self._max_torque[i] * actions[i]
 
         indices = torch.arange(self._sawyer.count, dtype=torch.int32, device=self._device)
         self._cartpoles.set_joint_efforts(forces, indices=indices)
@@ -133,6 +143,7 @@ class SawyerTask(BaseTask):
         dof_pos = self._sawyer.get_joint_positions()
         dof_vel = self._sawyer.get_joint_velocities()
         hand_pos = self._hand.get_world_pose()
+        hand_vel = self._hand.get_linear_velocity()
 
         # collect pole and cart joint positions and velocities for observation
         j0_pos = dof_pos[:, self._j0_dof_idx]
@@ -171,42 +182,64 @@ class SawyerTask(BaseTask):
         self.obs[:, 18] = hand_pos[0][0]
         self.obs[:, 19] = hand_pos[0][1]
         self.obs[:, 20] = hand_pos[0][2]
+        self.obs[:, 21] = hand_vel[0]
+        self.obs[:, 22] = hand_vel[1]
+        self.obs[:, 23] = hand_vel[2]
+
         return self.obs
 
     def calculate_metrics(self) -> None:
-        j0_pos = self.obs[:, 0]
-        j0_vel = self.obs[:, 1]
-        j1_pos = self.obs[:, 2]
-        j1_vel = self.obs[:, 3]
-        j2_pos = self.obs[:, 4]
-        j2_vel = self.obs[:, 5]
-        j3_pos = self.obs[:, 6]
-        j3_vel = self.obs[:, 7]
-        j4_pos = self.obs[:, 8]
-        j4_vel = self.obs[:, 9]
-        j5_pos = self.obs[:, 10]
-        j5_vel = self.obs[:, 11]
-        j6_pos = self.obs[:, 12]
-        j6_vel = self.obs[:, 13]
+        j_vel = torch.zeros(7)
+        j_pos = torch.zeros(7)
 
-        hand_pos = np.zeros(3)
-        hand_pose = np.zeros(4)
+        j_pos[0] = self.obs[:, 0]
+        j_vel[0] = self.obs[:, 1]
+        j_pos[1] = self.obs[:, 2]
+        j_vel[1] = self.obs[:, 3]
+        j_pos[2] = self.obs[:, 4]
+        j_vel[2] = self.obs[:, 5]
+        j_pos[3] = self.obs[:, 6]
+        j_vel[3] = self.obs[:, 7]
+        j_pos[4] = self.obs[:, 8]
+        j_vel[4] = self.obs[:, 9]
+        j_pos[5] = self.obs[:, 10]
+        j_vel[5] = self.obs[:, 11]
+        j_pos[6] = self.obs[:, 12]
+        j_vel[6] = self.obs[:, 13]
 
-        hand_pose[0] = self.obs[:, 14]
-        hand_pose[1] = self.obs[:, 15]
-        hand_pose[2] = self.obs[:, 16]
-        hand_pose[3] = self.obs[:, 17]
+        hand_pos = torch.zeros(7)
 
-        hand_pos[0] = self.obs[:, 18]
-        hand_pos[1] = self.obs[:, 19]
-        hand_pos[2] = self.obs[:, 20]
+        hand_pos[0] = self.obs[:, 14]
+        hand_pos[1] = self.obs[:, 15]
+        hand_pos[2] = self.obs[:, 16]
+        hand_pos[3] = self.obs[:, 17]
+        hand_pos[4] = self.obs[:, 18]
+        hand_pos[5] = self.obs[:, 19]
+        hand_pos[6] = self.obs[:, 20]
+
+        hand_vel = torch.zeros(3)
+
+        hand_vel[0] = self.obs[:, 21]
+        hand_vel[1] = self.obs[:, 22] 
+        hand_vel[2] = self.obs[:, 23]
 
         # compute penalty based on joint velocities
+        time = get_current_time() - self.start_time
 
+        # get trajectories and errors
+        pd = self.trajectory.target_pose(time)
+        vd = self.trajectory.target_velocity(time)
+
+        pe = hand_pos - pd
+        ve = hand_vel - vd
 
         # compute reward based on gripper pose and position vs trajectory, use get_current_time() and self.start_time
+        klog = lambda x, l: 2 / (torch.exp(x * l) + torch.exp(-x * l))
+        reward = self.w * klog(pe, self.l) + (1 - self.w) * klog(ve, self.l)
 
         # compute penalty if dof limits near exceeded, use get_dof_limits
+        reward = torch.where(torch.max(torch.abs(j_vel)) > torch.ones_like(reward) * -2.0, 1, reward)
+
 
         # compute penalty if jacobian is near singular, use get_jacobians
 
@@ -221,25 +254,28 @@ class SawyerTask(BaseTask):
 
     def is_done(self) -> None:
         j_vel = torch.zeros(7)
+        j_pos = torch.zeros(7)
 
-        j0_pos = self.obs[:, 0]
+        j_pos[0] = self.obs[:, 0]
         j_vel[0] = self.obs[:, 1]
-        j1_pos = self.obs[:, 2]
+        j_pos[1] = self.obs[:, 2]
         j_vel[1] = self.obs[:, 3]
-        j2_pos = self.obs[:, 4]
+        j_pos[2] = self.obs[:, 4]
         j_vel[2] = self.obs[:, 5]
-        j3_pos = self.obs[:, 6]
+        j_pos[3] = self.obs[:, 6]
         j_vel[3] = self.obs[:, 7]
-        j4_pos = self.obs[:, 8]
+        j_pos[4] = self.obs[:, 8]
         j_vel[4] = self.obs[:, 9]
-        j5_pos = self.obs[:, 10]
+        j_pos[5] = self.obs[:, 10]
         j_vel[5] = self.obs[:, 11]
-        j6_pos = self.obs[:, 12]
+        j_pos[6] = self.obs[:, 12]
         j_vel[6] = self.obs[:, 13]
 
-        j_vel = torch.abs(j_vel)
+        limits = self._sawyer.get_dof_limits()
+
         # reset if sawyer joint velocities too high, dof limits exceeded, or trajectory finished
-        resets = torch.where(torch.max(j_vel) > self._reset_vel, 1, 0)
+        resets = torch.where(torch.max(torch.abs(j_vel)) > self._reset_vel, 1, 0)
+        torch.where()
 
         # reset the robot if cart has reached reset_dist or pole is too far from upright
         resets = torch.where(torch.abs(cart_pos) > self._reset_dist, 1, 0)
